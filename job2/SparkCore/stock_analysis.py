1624ae126939cbf325dc99c97bbafb24f2c72f00
Elisa Catena <elisacatena00@gmail.com>
2024-06-03 19:08:35 +0200

    Update stock_analysis.py

--- job2/SparkCore/stock_analysis.py
+++ job2/SparkCore/stock_analysis.py
@@ -28,45 +28,46 @@ def sort_and_calculate_stats(values):
     last_close = close_prices[-1]
     total_volume = sum(volumes)
 
-    # Calcolo della variazione percentuale
     percentual_variation = ((last_close - first_close) / first_close) * 100
 
     return (total_volume, first_close, last_close, percentual_variation)
 
-def calculate_overall_stats(data):
-    industry_data = {}
+def combine_stats(data1, data2):
+    return data1 + data2
 
-    for key, values in data.items():
-        sector, industry, year, ticker = key
-        if (sector, industry, year) not in industry_data:
-            industry_data[(sector, industry, year)] = {}
-        industry_data[(sector, industry, year)][ticker] = sort_and_calculate_stats(values)
+def calculate_overall_stats(key, values):
+    sector, industry, year = key
+    tickers = {}
+    for value in values:
+        ticker, stats = value
+        if ticker not in tickers:
+            tickers[ticker] = []
+        tickers[ticker].append(stats)
+    
+    max_volume = 0
+    max_ticker_volume = None
+    max_increase = -float('inf')
+    max_ticker_increase = None
+    industry_first_close_sum = 0
+    industry_last_close_sum = 0
 
-    results = []
-    for (sector, industry, year), tickers in industry_data.items():
-        max_volume = 0
-        max_ticker_volume = None
-        max_increase = -float('inf')
-        max_ticker_increase = None
-        industry_first_close_sum = 0
-        industry_last_close_sum = 0
+    for ticker, ticker_values in tickers.items():
+        sorted_stats = sort_and_calculate_stats(ticker_values)
+        total_volume, first_close, last_close, percentual_variation = sorted_stats
 
-        for ticker, (total_volume, first_close, last_close, percentual_variation) in tickers.items():
-            industry_first_close_sum += first_close
-            industry_last_close_sum += last_close
+        industry_first_close_sum += first_close
+        industry_last_close_sum += last_close
 
-            if total_volume > max_volume:
-                max_volume = total_volume
-                max_ticker_volume = ticker
+        if total_volume > max_volume:
+            max_volume = total_volume
+            max_ticker_volume = ticker
 
-            if percentual_variation > max_increase:
-                max_increase = percentual_variation
-                max_ticker_increase = ticker
+        if percentual_variation > max_increase:
+            max_increase = percentual_variation
+            max_ticker_increase = ticker
 
-        industry_price_change = ((industry_last_close_sum - industry_first_close_sum) / industry_first_close_sum) * 100
-        results.append((sector, year, industry, round(industry_price_change, 2), max_ticker_increase, round(max_increase, 2), max_ticker_volume, max_volume))
-
-    return results
+    industry_price_change = ((industry_last_close_sum - industry_first_close_sum) / industry_first_close_sum) * 100
+    return (sector, year, industry, round(industry_price_change, 2), max_ticker_increase, round(max_increase, 2), max_ticker_volume, max_volume)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--input_path", type=str, help="Input dataset path")
@@ -81,30 +82,26 @@ spark = SparkSession \
     .getOrCreate()
 
 lines = spark.sparkContext.textFile(dataset_filepath).cache()
-data = lines.map(parse_line).filter(lambda x: x is not None).groupByKey().mapValues(list).collectAsMap()
+data = lines.map(parse_line).filter(lambda x: x is not None)
 
-results = calculate_overall_stats(data)
+grouped_data = data.map(lambda x: ((x[0][0], x[0][1], x[0][2]), (x[0][3], (x[1][0], x[1][1], x[1][2])))) \
+                   .groupByKey() \
+                   .mapValues(list)
 
-# Converte i risultati in un RDD e salva
-output = spark.sparkContext.parallelize(results)
+results = grouped_data.map(lambda x: calculate_overall_stats(x[0], x[1]))
 
-# Ordina l'output in base al settore e alla variazione percentuale in modo decrescente
-output_sorted = output.sortBy(lambda x: (x[0], -x[3]))
+output = results.sortBy(lambda x: (x[0], -x[3]))
 
-# Funzione per formattare le righe di output come richiesto
 def format_output(row):
     sector, year, industry, industry_price_change, max_ticker_increase, max_increase, max_ticker_volume, max_volume = row
     return f'"{sector}",{year},"{industry}",{industry_price_change},{max_ticker_increase} ({max_increase}),{max_ticker_volume} ({max_volume})'
 
-# Formatta l'output
-formatted_output = output_sorted.map(format_output)
+formatted_output = output.map(format_output)
 
-# Aggiungi l'intestazione
 header = "Sector, Year, Industry, Industry price change %, Max increase ticker (increase %), Max volume ticker (volume)"
 formatted_output_with_header = spark.sparkContext.parallelize([header]) \
     .union(formatted_output)
 
-# Riduci il numero di partizioni a 1 prima di salvare l'output
 formatted_output_with_header.coalesce(1).saveAsTextFile(output_filepath)
 
 spark.stop()
