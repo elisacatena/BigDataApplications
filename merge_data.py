f92d70e434ab7cfe652e6790ae24f41a901c81cf
Elisa Catena <93096381+elisacatena@users.noreply.github.com>
2024-06-05 17:23:00 +0200

    Update merge_data.py

--- merge_data.py
+++ merge_data.py
@@ -3,10 +3,10 @@ import pandas as pd
 # df_1 = pd.read_csv("/Users/elisacatena/Desktop/historical_stocks/historical_stock_prices.csv")
 # df_2 = pd.read_csv("/Users/elisacatena/Desktop/historical_stocks/historical_stocks.csv")
 
-df_1 = pd.read_csv("/Users/elisacatena/Desktop/historical_stock_prices1.csv")
-df_2 = pd.read_csv("/Users/elisacatena/Desktop/historical_stocks1.csv")
+df_1 = pd.read_csv("/Users/elisacatena/Desktop/historical_stock_prices.csv")
+df_2 = pd.read_csv("/Users/elisacatena/Desktop/historical_stocks.csv")
 
 merged_data = pd.merge(df_1, df_2, on='ticker')
 
 
-merged_data.to_csv("/Users/elisacatena/Desktop/merged_data1.csv", index=False)
+merged_data.to_csv("/Users/elisacatena/Desktop/merged_data.csv", index=False)
