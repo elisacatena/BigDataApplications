e5e6d159dc5e4179df607a6cec2cf64b898eaac1
Elisa Catena <elisacatena00@gmail.com>
2024-06-10 11:59:44 +0200

    Update reducer.py

--- job3/MapReduce/reducer.py
+++ job3/MapReduce/reducer.py
@@ -14,12 +14,11 @@ def main():
     ticker_data = defaultdict(lambda: defaultdict(list))
 
     for tokens in read_mapper_output(sys.stdin):
-        ticker, name, year, date, close_price = tokens
+        ticker, year, date, close_price = tokens
         year = int(year)
         close_price = float(close_price)
-        if year >= 2000:
-            ticker_data[ticker][year].append((date, close_price))
-
+        ticker_data[ticker][year].append((date, close_price))
+        
     for ticker, years in ticker_data.items():
         sorted_years = sorted(years.keys())
         for i in range(len(sorted_years) - 2):
@@ -32,7 +31,7 @@ def main():
                 last_date, last_close = price_data[-1]
                 percent_change = ((last_close - first_close) / first_close) * 100
                 percent_changes.append(round(percent_change,2))
-            print(f"{ticker} {three_years} {percent_changes}")
+            print(f"{ticker}\t{three_years}\t{percent_changes}")
 
 if __name__ == "__main__":
     main()
