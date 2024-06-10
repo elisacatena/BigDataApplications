8eac626c4746870a7b13dc8996423c2a780cac8b
Elisa Catena <elisacatena00@gmail.com>
2024-06-10 12:56:14 +0200

    Update reducer.py

--- job3/MapReduce/reducer.py
+++ job3/MapReduce/reducer.py
@@ -10,8 +10,9 @@ def read_mapper_output(file):
 
 def main():
 
-    # A dictionary to store prices for each ticker and year
+    # Dizionario per memorizzare i prezzi per ciascun ticker e anno
     ticker_data = defaultdict(lambda: defaultdict(list))
+    # Dizionario per memorizzare le variazioni percentuali
     group_changes = defaultdict(lambda: defaultdict(list))
 
     for tokens in read_mapper_output(sys.stdin):
@@ -22,25 +23,26 @@ def main():
         
     for ticker, years in ticker_data.items():
         sorted_years = sorted(years.keys())
+        # Considera gruppi di tre consecutivi
         for i in range(len(sorted_years) - 2):
             three_years = sorted_years[i:i+3]
             percent_changes = []
+            # Calcola la variazione percentuale per ciascun anno nel gruppo
             for year in three_years:
-                # Sort by date
                 price_data = sorted(years[year])
                 _, first_close = price_data[0]
                 _, last_close = price_data[-1]
                 percent_change = ((last_close - first_close) / first_close) * 100
                 percent_changes.append(round(percent_change,2))
-            group_key = tuple(percent_changes)
-            group_changes[group_key][ticker] = three_years
+            group_percent_changes = tuple(percent_changes)
+            group_changes[group_percent_changes][ticker] = three_years
 
     print("{:<30}\t{:<30}\t{:<30}".format("Tickers", "Years", "Percent Changes"))
-    for group_key, ticker_years in group_changes.items():
+    for group_percent_changes, ticker_years in group_changes.items():
         if len(ticker_years) >= 2:
             tickers = ", ".join(str(ticker) for ticker in ticker_years.keys()).replace(" ","")
             years = ", ".join(str(year) for year in list(ticker_years.values())[0])
-            percent_changes = "%, ".join(str(change) for change in group_key)
+            percent_changes = "%, ".join(str(change) for change in group_percent_changes)
             print("{:<30}\t{:<30}\t{:<30}".format(tickers, years, percent_changes+'%'))
 
 if __name__ == "__main__":
