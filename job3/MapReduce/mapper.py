4c9e1f98f3c53516809fc414dc2a955fae73a0c5
Elisa Catena <elisacatena00@gmail.com>
2024-06-10 11:59:47 +0200

    Update mapper.py

--- job3/MapReduce/mapper.py
+++ job3/MapReduce/mapper.py
@@ -14,12 +14,11 @@ def main():
         if row[0] == 'ticker':
             continue  # skip header
         ticker = row[0]
-        name = row[9]
         date = row[7]
         year = datetime.strptime(date, "%Y-%m-%d").year
         close_price = float(row[2])
-        
-        print(f"{ticker:<10}\t{name:<40}\t{year:<4}\t{date:<10}\t{close_price:<16}")
+        if year >= 2000:
+            print(f"{ticker:<10}\t{year:<4}\t{date:<10}\t{close_price:<16}")
 
 if __name__ == "__main__":
     main()
